7e3019e69560bc5678c35201026e6f9420ba2117
tegarsubkhan236 <tegarsubkhan236@gmail.com>
2024-09-19 15:12:55 +0700

    finish up endpoint to retrieve users

---
+++ src/main/java/com/example/spring_jwt/controllers/UserController.java
@@ -0,0 +1,35 @@
+package com.example.spring_jwt.controllers;
+
+import com.example.spring_jwt.entities.User;
+import com.example.spring_jwt.services.UserService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RequestMapping(name = "/users")
+@RestController
+public class UserController {
+    private final UserService userService;
+
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @GetMapping(name = "/me")
+    public ResponseEntity<User> authenticatedUser() {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        User currentUser = (User) authentication.getPrincipal();
+        return ResponseEntity.ok(currentUser);
+    }
+
+    @GetMapping(name = "/")
+    public ResponseEntity<List<User>> allUser() {
+        List<User> users = userService.allUsers();
+        return ResponseEntity.ok(users);
+    }
+}
---
+++ src/main/java/com/example/spring_jwt/services/UserService.java
@@ -0,0 +1,23 @@
+package com.example.spring_jwt.services;
+
+import com.example.spring_jwt.entities.User;
+import com.example.spring_jwt.repositories.UserRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+public class UserService {
+    private final UserRepository userRepository;
+
+    public UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    public List<User> allUsers() {
+        List<User> users = new ArrayList<>();
+        userRepository.findAll().forEach(users::add);
+        return users;
+    }
+}
