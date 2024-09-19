f66f2904d4085cb3af771b9d8f03a299d9659901
tegarsubkhan236 <tegarsubkhan236@gmail.com>
2024-09-19 15:34:34 +0700

    fix endpoint to retrieve users

--- src/main/java/com/example/spring_jwt/controllers/UserController.java
+++ src/main/java/com/example/spring_jwt/controllers/UserController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
-@RequestMapping(name = "/users")
+@RequestMapping("/users")
 @RestController
 public class UserController {
     private final UserService userService;
